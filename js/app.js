6026f1b4caf557fc5dc3015196d9cd26e4f66fc2
Architap18 <architapal1806@gmail.com>
2026-03-31 10:30:26 +0530

    updated search feature

--- js/app.js
+++ js/app.js
@@ -1,41 +1,80 @@
-
-const searchInput = document.getElementById("searchInput");   //search input element
-
-async function handleSearch() {
-  const query = searchInput.value;
-  showLoader();
-  const movies = await fetchMovies(query);
-  if (movies) {
-    renderMovies(movies);
-  } else {
-    showError("No movies found");
-  }
-}
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.querySelector("button");
+let allMovies = [];
+let display=[]; // a modified arrat to store the current data
+//trending movies
 window.onload = async () => {
   showLoader();
   const movies = await fetchTrending();
   if (movies) {
-    renderTrending(movies);
+    allMovies = movies;
+    display = movies;
+    renderTrending(display);
   } else {
-    showError("Failed to load trending");
+    showError("Failed to load movies");
   }
 };
-
-const searchBtn = document.querySelector("button");
-
+// searching
+searchInput.addEventListener("input", () => {
+  const query = searchInput.value.toLowerCase();
+  if (query === "") {
+    display = allMovies;
+  } else {
+    display = allMovies.filter(movie =>
+      movie.title.toLowerCase().includes(query)
+    );
+  }
+  renderTrending(display);
+});
+//search button
 searchBtn.addEventListener("click", async () => {
   const query = searchInput.value.trim();
-
-  if (!query)
-    {return;}
-
+  if (!query) {
+    return;
+  }
   showLoader();
-
   const movies = await fetchMovies(query);
-
   if (movies) {
     renderMovies(movies);
   } else {
     showError("No movies found");
   }
+});
+//yearwise
+document.getElementById("yearFilter").addEventListener("change", () => {
+  const year = document.getElementById("yearFilter").value;
+  display= allMovies
+  .filter(movie =>year === "" || movie.release_date?.startsWith(year));
+  renderTrending(display);
+});
+//sorting
+document.getElementById("sort").addEventListener("change", () => {
+  const option = document.getElementById("sort").value;
+
+  let sorted = [...display]; 
+
+  if (option === "az") {
+    sorted.sort((a, b) => a.title.localeCompare(b.title));
+  }
+
+  if (option === "year") {
+    sorted.sort((a, b) => {
+      const dateA = new Date(a.release_date || "2000");
+      const dateB = new Date(b.release_date || "2000");
+      return dateB - dateA;
+    });
+  }
+
+  display = sorted;
+  renderTrending(display);
+});
+
+const themeBtn = document.getElementById("toggle");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+
+  // Optional: change icon
+  themeBtn.textContent =
+    document.body.classList.contains("light-mode") ? "☀️" : "🌙";
 });
--- js/ui.js
+++ js/ui.js
@@ -1,36 +1,15 @@
-// ui.js
-function renderMovies(movies) {
-  const container = document.querySelector(".trending-list");
-  container.innerHTML = "";
-
-  movies.forEach(movie => {
-    const card = document.createElement("div");
-    card.classList.add("movie-card");
-    const poster = movie.Poster !== "N/A"? movie.Poster:"assets/background.jpg";
-    card.innerHTML = `<div class="poster-container">
-        <img src="${poster}"/>
-        <button class="heart-btn" onclick='addToWatchlist(${JSON.stringify(movie)})'>❤️</button>
-      </div>
-      <h3>${movie.Title}</h3>
-      <p>${movie.Year}</p>
-    `;
-
-    container.appendChild(card);
-  });
-}
-
-
 function renderTrending(movies) {
   const container = document.querySelector(".trending-list");
   container.innerHTML = "";
-
   movies.forEach(movie => {
     const card = document.createElement("div");
     card.classList.add("movie-card");
 
     const img = movie.poster_path? `https://image.tmdb.org/t/p/w500${movie.poster_path}`: "assets/background.jpg";
-    card.innerHTML = `<div class="poster-container">
-        <img src="${img}">
+
+    card.innerHTML = `
+      <div class="poster-container">
+        <img src="${img}" onerror="this.src='assets/background.jpg'">
         <button onclick='addToWatchlist(${JSON.stringify(movie)})'>ADD</button>
       </div>
       <h3>${movie.title}</h3>
@@ -40,9 +19,36 @@ function renderTrending(movies) {
     container.appendChild(card);
   });
 }
+
+function renderMovies(movies) {
+  const container = document.querySelector(".trending-list");
+  container.innerHTML = "";
+
+  movies.forEach(movie => {
+    const card = document.createElement("div");
+    card.classList.add("movie-card");
+
+    const poster = movie.Poster !== "N/A"
+      ? movie.Poster
+      : "assets/background.jpg";
+
+    card.innerHTML = `
+      <div class="poster-container">
+        <img src="${poster}" onerror="this.src='assets/background.jpg'">
+        <button onclick='addToWatchlist(${JSON.stringify(movie)})'>ADD</button>
+      </div>
+      <h3>${movie.Title}</h3>
+      <p>${movie.Year}</p>
+    `;
+
+    container.appendChild(card);
+  });
+}
+
 function showLoader() {
   document.querySelector(".trending-list").innerHTML = "Loading...";
 }
+
 function showError(msg) {
   document.querySelector(".trending-list").innerHTML = msg;
 }
